5bbd00f890a20332a4d87f9bbea9c4a1efea638a
Chris Millar <cmillar@adobe.com>
2022-04-06 14:21:35 -0600

    Featured Posts

---
+++ blocks/featured-posts/featured-posts.js
@@ -0,0 +1,22 @@
+export default async function init(el) {
+    const resp = await fetch('/query-index.json');
+    console.log(resp.status);
+    if (resp.status == 200) {
+        const json = await resp.json();
+        const list = document.createElement('ul');
+        json.data.forEach(item => {
+            const post = document.createElement('li');
+            post.className = 'post';
+            const link = document.createElement('a');
+            link.href = item.path;
+            link.textContent = item.title;
+
+
+            post.append(link);
+            list.append(post);
+        });
+
+
+        el.append(list);
+    }
+}
